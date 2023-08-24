6f9b9af297ae811d47622c73edd262353826826c
Wisdom Okogho <91355531+setgram@users.noreply.github.com>
2023-08-24 11:17:48 +0100

    add share copy functionality

--- my-project/src/page/Listing.jsx
+++ my-project/src/page/Listing.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { FaShare } from "react-icons/fa";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import Spinner from "../Spinner";
@@ -9,6 +10,7 @@ import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 export default function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   const params = useParams();
 
   useEffect(() => {
@@ -51,6 +53,23 @@ export default function Listing() {
           </SplideSlide>
         ))}
       </Splide>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
